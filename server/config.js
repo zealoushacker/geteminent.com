2a34589988b8a2b9253eb1336d80647fd6c22853
Alex Notov <anotov@gmail.com>
2012-12-28 14:34:17 -0800

    Split off config into its own file

---
+++ server/config.js
@@ -0,0 +1,9 @@
+Accounts.loginServiceConfiguration.remove({
+  service: "facebook"
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: "facebook",
+  appId: process.env.FACEBOOK_APP_ID,
+  secret: process.env.FACEBOOK_APP_SECRET
+});
