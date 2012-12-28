ad1bf149a6314592bb7f92d7c04bdfa124d41e73
Alex Notov <anotov@gmail.com>
2012-12-28 14:43:56 -0800

    Remove redundant code from server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,2 @@
 Meteor.startup(function () {
-  Accounts.loginServiceConfiguration.remove({
-    service: "facebook"
-  });
-
-  Accounts.loginServiceConfiguration.insert({
-    service: "facebook",
-    appId: process.env.FACEBOOK_APP_ID,
-    secret: process.env.FACEBOOK_APP_SECRET
-  });
 });
